5270a265cacdd09b4a5ab191b4171e06d46aeda9
YuzhikMariya <47949650+YuzhikMariya@users.noreply.github.com>
2020-08-07 15:16:43 +0300

    task1

---
+++ Array Processing Tool/js/formHandler.js
@@ -0,0 +1,44 @@
+let form_subSum = document.querySelector('#form_subSum');
+let form_search = document.querySelector('#form_search');
+let form_select = document.querySelector('#form_selection');
+
+let arrTool = new ArrayProcessingTool();
+
+form_subSum.onsubmit = () => {
+    let arr = document.querySelector('#arr_subSum')
+    .value
+    .split(', ')
+    .map(i => parseInt(i));
+
+    document.querySelector('#answer_subSum').innerHTML = 
+        "Sub sum O(n<sup>2</sup>) = " + arrTool.getMaxSubSum_long(arr) + "<br/>" +
+        "Sub sum O(n) = " + arrTool.getMaxSubSum_short(arr);
+    return false;
+};
+
+form_search.onsubmit = () => {
+    let arr = document.querySelector('#arr_search')
+    .value
+    .split(', ')
+    .map(i => parseInt(i));
+
+    document.querySelector('#answer_search').innerHTML = 
+        "Min = " + arrTool.getMin(arr) + "<br/>" +
+        "Max = " + arrTool.getMax(arr) + "<br/>" +
+        "Meduim = " + arrTool.getMedium(arr);
+    return false;
+};
+
+form_select.onsubmit = () => {
+    let arr = document.querySelector('#arr_selection')
+    .value
+    .split(', ')
+    .map(i => parseInt(i));
+
+    document.querySelector('#answer_selection').innerHTML = 
+        "Selection = [" + arrTool.getSelection(arr) + "]";
+    return false;
+};
+
+
+
