a1dce2a6141da86a5facc36250e22fdeb6f95b34
YuzhikMariya <47949650+YuzhikMariya@users.noreply.github.com>
2020-08-12 11:56:19 +0300

    task 5

---
+++ Array Sorter/js/arrayHandler.js
@@ -0,0 +1,91 @@
+class ArraySorter{
+
+
+    swap(arr, indexA, indexB){
+        let temp = arr[indexA];
+        arr[indexA] = arr[indexB];
+        arr[indexB] = temp;
+    }
+
+    shakerSort(arr){
+        let left = 0;
+        let right = arr.length - 1;
+        while (left <= right) {
+            for (let i = right; i > left; i--){
+                if (arr[i - 1] > arr[i]){
+                    this.swap(arr, i, i-1);
+                }
+            }
+            left++;
+            for (let i = left; i < right; i++){
+                if (arr[i] > arr[i + 1]){
+                    this.swap(arr, i, i+1);
+                }
+            }
+            right--;
+        } 
+        return arr;
+    }
+
+    insertSort(arr){
+        for (let i = 1; i < arr.length; i++) {
+            let x = arr[i];
+            let j = i;
+            while ((j > 0) && (arr[j - 1] > x)) {
+                arr[j] = arr[j - 1];
+                j--;
+            }
+            arr[j] = x;
+        }
+        return arr;
+    }
+
+    selectionSort(arr){
+        
+        for (let i = 0; i < arr.length - 1; i++) {
+            let indexMin = i;
+            for (let j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[indexMin] ) {
+                    indexMin = j;
+                }
+            }
+            if (indexMin !== i) {
+                this.swap(arr, i, indexMin);
+            }
+        }
+        return arr;
+    }
+
+    merge(arrFirst, arrSecond){
+        const arrSort = [];
+        let i = 0, j = 0;
+
+        while (i < arrFirst.length && j < arrSecond.length) {
+            arrSort.push(
+                (arrFirst[i] < arrSecond[j]) ?
+                    arrFirst[i++] : arrSecond[j++]
+            );
+        }
+
+        return [
+            ...arrSort,
+            ...arrFirst.slice(i),
+            ...arrSecond.slice(j)
+        ];
+    }
+
+    mergeSort(arr){
+
+        if (arr.length <= 1) {
+            return arr;
+        }
+
+        const middle = Math.floor(arr.length / 2);
+        const arrLeft = arr.slice(0, middle);
+        const arrRight = arr.slice(middle);
+
+        return this.merge(this.mergeSort(arrLeft), this.mergeSort(arrRight));;
+    }
+
+
+}
---
+++ Array Sorter/js/formHandler.js
@@ -0,0 +1,38 @@
+let form = document.querySelector('form');
+
+form.onsubmit = () => {
+    let arrayOfStr = document.querySelector('#array').value.split(' ');
+    let array = arrayOfStr.map((i) => parseInt(i));
+
+    let type = document.querySelector('#type').value;
+
+    const arrayHandler = new ArraySorter();
+    let ascendingArray;
+
+    switch(type){
+        case "Shaker":
+            ascendingArray = arrayHandler.shakerSort(array);
+            break;
+        case "Insert":
+            ascendingArray = arrayHandler.insertSort(array);
+            break;
+        case "Selection":
+            ascendingArray = arrayHandler.selectionSort(array);
+            break;
+        case "Merge":
+            ascendingArray = arrayHandler.mergeSort(array);
+            break;
+    }
+
+    let answer = document.querySelector('#answer');
+    if(document.querySelector('#direction').checked){
+        answer.textContent = ascendingArray.reverse();
+    }else{
+        answer.textContent = ascendingArray;
+    }
+    
+    
+    return false;
+
+    
+}
