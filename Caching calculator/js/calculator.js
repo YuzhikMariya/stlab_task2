2dba3f48f8393723773be73545bc66d246b64a7e
YuzhikMariya <47949650+YuzhikMariya@users.noreply.github.com>
2020-08-12 14:27:26 +0300

    task 7

---
+++ Caching calculator/js/calculator.js
@@ -0,0 +1,70 @@
+class CachingCalculator{
+
+    _cache = {};
+
+    _operations = {'+': (a, b) => a + b, 
+                '-': (a, b) => a - b, 
+                '*': (a, b) => a * b, 
+                '/': (a, b) => a / b, 
+                '^': (a, b) => Math.pow(a, b), 
+                'log': (a, b) => Math.log(b) / Math.log(a)
+    };
+
+    _cachedOperations = [];
+
+    registerOperations(operations){
+        this._cachedOperations = [];
+        operations.forEach(op => {
+            this._cachedOperations.push(op);
+        });
+    }
+
+	calculate(a, b, op) {
+        let cacheKey = a + op + b;
+        
+
+        if(this.isCachedOperation(op)){
+            let result = this.isCached(a, b, op);
+            if (  result ){
+                alert("from cache");
+            } else {
+                result = this._operations[op](a, b);
+                this._cache[cacheKey] = result;
+                
+            }
+            return result;
+
+        }else{
+            return this._operations[op](a, b);
+        }
+	}
+
+
+	isCached(a, b, op){
+		let result = null;
+		let searchedKeys = [a + op + b];
+
+		if (op === '+' || op === '*' ){
+			searchedKeys.push(b + op + a);
+		}
+
+		for (let key in searchedKeys) {
+            result = this._cache[searchedKeys[key]];
+			if (result){
+				break;
+			}
+		};
+
+		return result;
+    }
+    
+    isCachedOperation(op){
+        let result = false;
+        this._cachedOperations.forEach(el => {
+            if(el == op){
+                result = true;
+            }
+        });
+        return result;
+    }
+}
