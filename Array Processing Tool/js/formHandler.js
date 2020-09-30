af6257c05a3236fd2693df57624f2beb8da25b0b
YuzhikMariya <47949650+YuzhikMariya@users.noreply.github.com>
2020-09-30 11:57:56 +0300

    removed button highlighting when pressed

--- Array Processing Tool/js/formHandler.js
+++ Array Processing Tool/js/formHandler.js
@@ -5,38 +5,46 @@ let form_select = document.querySelector('#form_selection');
 let arrTool = new ArrayProcessingTool();
 
 form_subSum.onsubmit = () => {
-    let arr = document.querySelector('#arr_subSum')
-    .value
-    .split(',')
-    .map(i => parseInt(i));
+    if(document.querySelector('#arr_subSum').value != ''){
+        let arr = document.querySelector('#arr_subSum')
+        .value
+        .split(',')
+        .map(i => parseInt(i));
 
-    document.querySelector('#answer_subSum').innerHTML = 
+
+        document.querySelector('#answer_subSum').innerHTML = 
         "Sub sum O(n<sup>2</sup>) = " + arrTool.getMaxSubSum_long(arr) + "<br/>" +
         "Sub sum O(n) = " + arrTool.getMaxSubSum_short(arr);
+    }
+    
     return false;
 };
 
 form_search.onsubmit = () => {
-    let arr = document.querySelector('#arr_search')
-    .value
-    .split(', ')
-    .map(i => parseInt(i));
+    if(document.querySelector('#arr_search').value != ''){
+        let arr = document.querySelector('#arr_search')
+        .value
+        .split(',')
+        .map(i => parseInt(i));
 
-    document.querySelector('#answer_search').innerHTML = 
-        "Min = " + arrTool.getMin(arr) + "<br/>" +
-        "Max = " + arrTool.getMax(arr) + "<br/>" +
-        "Meduim = " + arrTool.getMedium(arr);
+        document.querySelector('#answer_search').innerHTML = 
+            "Min = " + arrTool.getMin(arr) + "<br/>" +
+            "Max = " + arrTool.getMax(arr) + "<br/>" +
+            "Meduim = " + arrTool.getMedium(arr);
+    }
     return false;
 };
 
 form_select.onsubmit = () => {
-    let arr = document.querySelector('#arr_selection')
-    .value
-    .split(', ')
-    .map(i => parseInt(i));
+    if(document.querySelector('#arr_selection').value != ''){
+        let arr = document.querySelector('#arr_selection')
+        .value
+        .split(',')
+        .map(i => parseInt(i));
 
-    document.querySelector('#answer_selection').innerHTML = 
-        "Selection = [" + arrTool.getSelection(arr) + "]";
+        document.querySelector('#answer_selection').innerHTML = 
+            "Selection = [" + arrTool.getSelection(arr) + "]";
+    }
     return false;
 };
 
